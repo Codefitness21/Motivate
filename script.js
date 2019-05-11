04e7d008d0e6b1eb4a403766a323d6cd3d6b568c
Kelly <pip2@Pips-MacBook-Pro.local>
2019-05-11 16:52:25 -0500

    build my motivation app

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 
+
+
+
+const quotes = [
+    "Lift the day! - Me",
+    "Excuses are the refuge of the defeated. - My dad", 
+    "Tough times don't last. Tough people do. - Eric Thomas", 
+    "Setback is a setup for a comeback. - Willie Jolley",
+    "New levels bring new devils. - Eric Thomas",
+    "Win the day. Me",
+    "Green light the day- Me"
+]
+ 
+function motivate () {
+    let numberQuotes = quotes.length;
+    let randomDecimal= Math.random();
+    let randomNumberwithDecimal = randomDecimal * numberQuotes;
+    let randomInteger = Math.floor(randomNumberwithDecimal);
+    document.getElementById("displayText").innerHTML = quotes[randomInteger];
+    console.log("button is working");
+}
+
